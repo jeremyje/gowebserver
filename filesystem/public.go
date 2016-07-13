3edd5b9cbbba29b52e515ebf6fbe4a321332ea8d
Futon Redemption <futonredemption@gmail.com>
2016-07-13 22:21:42 +0000

    Hookup support for zip hosting.

--- filesystem/public.go
+++ filesystem/public.go
@@ -2,8 +2,14 @@ package filesystem
 
 import (
 	"net/http"
+	"strings"
 )
 
 func New(path string) (http.FileSystem, error) {
+	lcPath := path
+	if strings.HasSuffix(lcPath, ".zip") {
+		handler, _, _, err := newZipFs(path)
+		return handler, err
+	}
 	return newNative(path), nil
 }
